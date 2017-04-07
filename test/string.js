7874a1e71f894645532f6f8300fc8b09293a48e9
kejiayi <760220261@qq.com>
2017-04-07 11:16:32 +0800

    删除string文件

--- test/string.js
+++
@@ -1,8 +0,0 @@
-//import { expect, assert } from 'chai';
-//describe('字符串扩展', function() {
-//	it('padstart,padend', function() {
-//		var result = 'ke'.padStart(5, 'ji');
-//		assert.equal(result, 'jiajke')
-//	});
-//
-//});
