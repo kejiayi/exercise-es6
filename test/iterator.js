4789d9c17234424298a3b1916edc8532fc50ac0b
kejiayi <760220261@qq.com>
2017-04-07 21:33:10 +0800

    iterator 测试

--- test/iterator.js
+++ test/iterator.js
@@ -1,6 +1,30 @@
 import { expect, assert } from 'chai';
 import 'babel-polyfill';
 
-describe('', function() {
+describe('iterator', function() {
+	it('类部署iterator接口', function() {
+		class newClass {
+			constructor(start, stop) {
+					this.value = start;
+					this.stop = stop;
+				}
+				[Symbol.iterator]() { return this; }
+			next() {
+				var value = this.value;
+				if(value < this.stop) {
+					this.value++;
+					return { done: false, value: value }
+				}
+				return { done: true, value: undefined }
+			}
 
+		}
+
+		function range(start, stop) {
+			return new newClass(start, stop);
+		}
+		for(let value of range(2, 6)) {
+			console.log(value);
+		}
+	})
 });
